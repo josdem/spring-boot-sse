05d0c9a387bc1e25fb0b460c7c37ca506897e484
josdem <joseluis.delacruz@gmail.com>
2019-04-24 15:30:31 -0400

    Adding Command Message

---
+++ src/main/java/com/jos/dem/springboot/model/MessageCommand.java
@@ -0,0 +1,17 @@
+package com.jos.dem.springboot.sse.model;
+
+import java.time.Instant;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageCommand implements Command {
+	private String nickname;
+	private String text;
+  private Instant timestamp;
+}
+
